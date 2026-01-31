232c41a697cc2c3124e4439dc2be8b3557b23227
Milton Munoz <livemilton@gmail.com>
2026-01-31 15:53:42 -0500

    fullstack frontend :sparkles: Adding edit button

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,6 @@
+import { Link, useNavigate } from "react-router-dom"
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
-
 
 type ProductDetailsProps = {
     product: Product
@@ -22,7 +22,10 @@ export default function ProductDetails({product} : ProductDetailsProps) {
         </td>
         <td className="p-3 text-lg text-gray-800">
               <div className="flex gap-2 items-center">
-                <button>Editar</button>  
+                <Link
+                  to={`/productos/${product.id}/editar`}    
+                  className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"            
+                >Editar</Link>  
               </div>
         </td>
     </tr>
