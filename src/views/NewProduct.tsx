e297e666270fb2447155b83af4eb8119c1cc5347
Milton Munoz <livemilton@gmail.com>
2026-01-31 11:42:02 -0500

    fullstack frontend :sparkles: Adding Product to the database from React

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,5 +1,5 @@
 
-import { Link, Form, useActionData, type ActionFunctionArgs } from "react-router-dom";
+import { Link, Form, useActionData, type ActionFunctionArgs, redirect } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
 
@@ -19,9 +19,9 @@ export async function action({request}: ActionFunctionArgs){
     return error
   }
 
-  addProduct(data)
+  await addProduct(data)
 
-  return{}
+  return redirect('/')
 }
 
 export default function NewProduct() {
