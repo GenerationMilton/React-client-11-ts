3056fd4900d7098d44b20a95e09ffe8321fc7417
Milton Munoz <livemilton@gmail.com>
2026-01-30 19:39:14 -0500

    fullstack frontend :sparkles: What are Data router actions

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Products from "./views/Products";
-import NewProduct from "./views/NewProduct";
+import NewProduct, { action as newProductAction } from "./views/NewProduct";
 
 
 export const router = createBrowserRouter ([
@@ -13,9 +13,10 @@ export const router = createBrowserRouter ([
                 index: true,
                 element: <Products />
             },
-               {
+            {
                 path: 'productos/nuevo',
-                element: <NewProduct />
+                element: <NewProduct />,
+                action: newProductAction
             },
         ]
     }
